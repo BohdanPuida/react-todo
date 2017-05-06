72c7fb6b213217fcdbd0158c9724baf12cb283d5
waskannichtun <bogdanpuida529@gmail.com>
2017-05-07 00:30:45 +0300

    Add filter func

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -58,4 +58,50 @@ describe('TodoAPI', () => {
     });
 
   });
-})
+
+  describe('filterTodos', () =>{
+    var todos = [{
+      id:1,
+      text: 'Show text here',
+      completed:true
+    }, {
+      id: 2,
+      text: 'Save text here',
+      completed: false
+
+    },{
+      id: 3,
+      text: 'Some text here',
+      completed: true
+      }];
+
+      it("should return all items if showCompleted is true",()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      });
+
+      it('should return non-completed todos when showCompleted is false',()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+        expect(filteredTodos.length).toBe(1);
+      });
+
+      it('should sort by completed status',()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos,true, '');
+        expect(filteredTodos[0].completed).toBe(false);
+
+      });
+
+      it("should filter todos by a search text",()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+        expect(filteredTodos.length).toBe(1);
+      });
+
+      it("should return all todos if search text is empty",()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      });
+
+
+
+  });
+});
